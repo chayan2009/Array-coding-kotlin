e6371f454e48412ef34bda2e55ecaf7d589ff686
chayan <chayanchowdhury@gmail.com>
2024-08-24 19:35:30 +0530

    Find the Maximum and Minimum Elements in an Array

---
+++ MaxAndMinimumElementFromArray.kt
@@ -0,0 +1,31 @@
+fun main() {
+    var array=intArrayOf(10,20,32,45,78,232,892)
+    var maxNumber=findMaxElementFromArray(array)
+    println("maximum element in an Array::  $maxNumber")
+    var minNumber=findMinElementFromArray(array)
+    println("Minimum element in an Array::  $minNumber")
+}
+
+   fun findMinElementFromArray(arr:IntArray):Int{
+
+    var minimum=arr[0]
+    for(i in arr){
+        if(i<minimum){
+            minimum=i
+        }
+    }
+    return minimum
+   }
+
+   
+   fun findMaxElementFromArray(arr:IntArray):Int{
+
+     var maximum= arr[0]
+
+     for(i in arr){
+        if(i>maximum)
+        maximum=i
+     }
+     return maximum
+
+    }
