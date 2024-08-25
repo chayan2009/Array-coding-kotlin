3925f36b5405e06da06add73d65402d624e48310
chayan <chayanchowdhury@gmail.com>
2024-08-26 00:07:20 +0530

    Check if Two Arrays are Equal

--- CheckTwoArraysAreEqual.kt
+++ CheckTwoArraysAreEqual.kt
@@ -1,8 +1,30 @@
  fun main() {
-    print("Check if Two Arrays are Equal")
     var array1=intArrayOf(1,2,3,4)
-    var array2=intArrayOf(1,2,3,4,5)
+    var array2=intArrayOf(1,2,3,4)
+
+    //Apprach1
     println("Are equal : ${areArraysEqual(array1,array2)}")
+
+    //Approach2
+  //  println("Are equal loop : ${areArraysEqual1(array1,array2)}")
+
+}
+
+fun areArraysEqual1(array1:IntArray,array2:IntArray):Boolean{
+    var flag=false
+    if(array1.size==array2.size){
+        for(i in array1){
+            for(j in array2){
+                if(i==j){
+                    flag=true
+                }
+            }
+        }
+    }else{
+        flag=false
+    }
+   
+    return flag
 }
 
 fun areArraysEqual(array1:IntArray,array2:IntArray):Boolean{
