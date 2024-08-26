6a18e8cc4afc6cdbcde218e561c7f5cba182f521
chayan <chayanchowdhury@gmail.com>
2024-08-26 15:02:03 +0530

    Find the Intersection of Two Arrays

--- IntersectionOfTwoArrays.kt
+++ IntersectionOfTwoArrays.kt
@@ -2,10 +2,29 @@ fun main(){
     println("Find the Intersection of Two Arrays")
     var array1=intArrayOf(1,2,3,5)
     var array2=intArrayOf(1,3,7,8,9,9)
+    //approeach 1
     val result =checkIntersectionInTwoArray(array1,array2)
     println("Find the Intersection of Two Arrays:: ${result.joinToString(",")}")
-}
 
+     //approeach -2
+    val result1=checkIntersectionInTwoArrayusingiterrative(array1, array2)
+    println("Find the Intersection of Two Arrays:: ${result1.joinToString(",")}")
+
+    }
+
+    fun checkIntersectionInTwoArrayusingiterrative(arr:IntArray,arr1:IntArray):IntArray{
+
+        var reusltArray=mutableListOf<Int>()
+        for(i in arr.indices){
+            for(j in arr1.indices){
+                if(arr[i]==arr1[j]){
+                    reusltArray.add(arr[i])
+                }
+            }
+        }
+        return reusltArray.toIntArray()
+
+    }
 fun checkIntersectionInTwoArray(arr:IntArray,arr2:IntArray):IntArray{
         return arr.intersect(arr2.toSet()).toIntArray()
 }
