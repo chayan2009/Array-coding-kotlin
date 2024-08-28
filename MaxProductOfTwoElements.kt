0602e31718deb1d10c06fed7776fb8722675d80b
chayan <chayanchowdhury@gmail.com>
2024-08-28 19:40:28 +0530

    Find the Maximum Product of Two Elements

---
+++ MaxProductOfTwoElements.kt
@@ -0,0 +1,22 @@
+fun main(){
+    println("Find the Maximum Product of Two Elements")
+    var arr = intArrayOf(1,2,22,4,44,5,55,56,76,777,888,23)
+    println(maxProductOfTwoElements(arr))  // Output: 56 (7 * 8)
+
+}
+
+fun maxProductOfTwoElements(arr:IntArray):Int{
+    var max1=Int.MIN_VALUE
+    var max2=Int.MIN_VALUE
+
+    for(num in arr){
+        if(num>max1){
+            max2=max1
+            max1=num
+        }else if(num>max2){
+            max2=num
+        }
+        
+    }
+    return max1*max2
+}
